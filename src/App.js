b027dff345adc726f92f7aac1000f3ba50d37665
kdany25 <kabadany25@gmail.com>
2023-09-27 13:23:12 +0200

    feat: added login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 
 function App() {
-	return <div className="App">
-
-		<Home/>
-	</div>;
+	return (
+		<div className="App">
+			<Login />
+		</div>
+	);
 }
 
 export default App;
